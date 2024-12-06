ae1057c4d0e7760702e867380f6eb62745cb0f51
Ariel-1979 <arieldominguez001@gmail.com>
2024-12-06 19:32:54 -0300

    Cambio de Logos

--- components/hero-video/Hero.tsx
+++ components/hero-video/Hero.tsx
@@ -1,36 +1,44 @@
-import Link from "next/link";
-import { Button } from "../ui/button";
+import Image from "next/image";
 
 export default function Hero() {
   return (
-    <section className="relative min-h-screen flex items-center justify-center overflow-hidden">
+    <section className="relative min-h-screen flex items-center justify-center overflow-hidden ">
       <video
         className="absolute w-full h-full object-cover"
         autoPlay
         muted
         playsInline
+        loop
       >
         <source src="/banner.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
       {/* Overlay semitransparente */}
-      <div className="absolute inset-0 bg-black opacity-50"></div>
-      <div className="relative z-10 container px-4 md:px-6 text-center">
-        <div className="space-y-4 bg-black bg-opacity-30 p-6 rounded-lg backdrop-blur-sm">
+      <div className="absolute inset-0 bg-black opacity-25"></div>
+      <div className="relative z-10 container px-4 md:px-20 text-center">
+        <div /* className=" bg-black bg-opacity-25 pt-4 pb-4 rounded-2xl backdrop-blur-sm" */
+        >
+          <Image
+            src="/ProGPS_Logo.png"
+            alt="ProGPS"
+            width={200}
+            height={200}
+            className="mx-auto"
+          />
           <h1 className="text-3xl font-bold tracking-tighter text-[--dark-principal] sm:text-4xl md:text-5xl lg:text-6xl/none text-center shadow-text">
             Logística y Recupero Vehicular
           </h1>
-          <p className="mx-auto max-w-[700px] text-gray-100 md:text-xl shadow-text">
+          <p className="mx-auto max-w-[700px] text-bold md:text-xl shadow-text">
             Soluciones profesionales de rastreo GPS para tu flota vehicular con
             tecnología de última generación.
           </p>
-          <div className="space-x-4">
+          {/*   <div className="space-x-4">
             <Link href="#contacto">
               <Button className="bg-[--dark-principal] hover:bg-yellow-600 text-black font-bold">
                 Quiero recibir más información
               </Button>
             </Link>
-          </div>
+          </div> */}
         </div>
       </div>
     </section>
--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -4,10 +4,10 @@ export default function Logo() {
   return (
     <div className="flex items-center gap-2 text-black dark:text-white">
       {/* <MapPin className="h-8 w-8 text-[--light-primario] dark:text-[--dark-secundario]" /> */}
-      <Image src="/ProGPS_Logo.png" alt="ProGPS" width={45} height={42} />
-      <span className="font-bold text-xl text-[--light-primario] dark:text-[--dark-principal]">
-        ProGPS
-      </span>
+      <Image src="/ProGPS_Logo.png" alt="ProGPS" width={50} height={50} />
+      {/* <span className="font-bold  text-[--light-primario] dark:text-[--dark-principal]">
+        Logistica y Recupero Vehicular
+      </span> */}
     </div>
   );
 }
