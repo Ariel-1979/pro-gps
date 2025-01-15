b57256eb646807fe9569b67bc9f51a164d498169
Ariel-1979 <arieldominguez001@gmail.com>
2025-01-15 11:22:31 -0300

    Login Link

--- components/navbar-menu/LINKS.js
+++ components/navbar-menu/LINKS.js
@@ -2,5 +2,5 @@ export const LINKS = [
   { href: "/#servicios", label: "Características" },
   { href: "/#contacto", label: "Contacto" },
   { href: "/#siniestro", label: "Emergencias" },
-  { href: "https://www.rastreo.progps.com.ar/login", label: "Iniciar Sesión" },
+  { href: "https://rastreo.progps.com.ar", label: "Iniciar Sesión" },
 ];
